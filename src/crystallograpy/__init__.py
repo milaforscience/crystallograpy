b19d763ca0494fe7a0ef0925b9008ee965d0954d
Alex <alexhg@e.email>
2026-06-08 14:43:22 -0400

    Create utils.py and move some content from __init__.py

--- src/crystallograpy/__init__.py
+++ src/crystallograpy/__init__.py
@@ -1,32 +1,4 @@
-import os
-from pathlib import Path
-
-import yaml
-
-# Path to the yaml directory relative to this __init__.py file
-YAML_DIR = Path(__file__).parent.parent.parent / "yaml"
-
-# Dictionary to store databases
-_databases = {"wyckoff.yaml": {}, "spacegroups.yaml": {}}
-
-
-def load_database(filename: str) -> dict:
-    """Load YAML database into memory."""
-    global _databases
-
-    # Do not reload if already loaded
-    if _databases[filename]:
-        return _databases[filename]
-
-    # Load YAML file
-    try:
-        with open(YAML_DIR / filename, "r") as f:
-            _databases[filename] = yaml.safe_load(f)
-    except Exception as e:
-        print(f"Warning: could not load {filename}: {e}")
-
-    return _databases[filename]
-
+from .utils import load_database
 
 # Load space group and Wyckoff data when the package is imported
 spacegroups = load_database("spacegroups.yaml")
